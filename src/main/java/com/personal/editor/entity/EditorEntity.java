b820b3522fec148c204d3decaa11660967c43cbb
SATYAKI SAHA <satyaki01saha@gmail.com>
2025-08-07 05:06:34 +0000

    Api SetUp

--- src/main/java/com/personal/editor/entity/EditorEntity.java
+++ src/main/java/com/personal/editor/entity/EditorEntity.java
@@ -1,7 +1,18 @@
 package com.personal.editor.entity;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("zipstore")
+@Data
 public class EditorEntity {
+    @Id
+    public String id;
+    private byte[] file;
+    private String userId;
+    private boolean forEvaluation;
+    private Integer language;
+    private Double score;
 }
+
